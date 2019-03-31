f8551661eed564fe836caa78b02dd829299ce97e
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-31 00:35:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,7 +31,7 @@ class Scraper
     student_links = {}
 
     social_media_links = student_profile.css(".social-icon-container a").each do |sm_links|
-    sm_links = a 
+    sm_links = a['href']
       binding.pry
     end
   end
