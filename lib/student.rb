c6d92a51fac754a4600700e0f5427d8b648524da
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-31 01:25:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
@@ -5,7 +6,9 @@ class Student
   @@all = []
 
   def initialize(student_hash)
+    student_hash.each do |variable|
 
+    end
 
     @@all << self
   end
