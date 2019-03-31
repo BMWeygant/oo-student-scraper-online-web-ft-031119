43b80f161afe94f83fcedb36b8fea62e3b6cd340
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-31 01:39:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student_hash|
       Student.new(student_hash)
-      binding.pry
+      #binding.pry
     end
   end
 
